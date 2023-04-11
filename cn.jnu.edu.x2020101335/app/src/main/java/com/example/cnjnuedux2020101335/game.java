0de2267e07402e9778332d865e549b5feb21d63f
1522164387 <1522164387@qq.com>
2023-04-11 22:07:44 +0800

    app02

---
+++ cn.jnu.edu.x2020101335/app/src/main/java/com/example/cnjnuedux2020101335/game.java
@@ -0,0 +1,10 @@
+package com.example.cnjnuedux2020101335;
+
+public class game {
+    public void roll(int pins) {
+    }
+
+    public int score() {
+        return 0;
+    }
+}
---
+++ cn.jnu.edu.x2020101335/app/src/test/java/com/example/cnjnuedux2020101335/BollingGameTest.java
@@ -0,0 +1,38 @@
+package com.example.cnjnuedux2020101335;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class BollingGameTest {
+
+    private game g;
+
+    @Before
+    public void setUp() {
+        g = new game();
+    }
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
+
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5);
+    }
+
+    @Test
+    public void test1()
+    {
+        rollMany(20,0);
+        assertEquals(0, g.score());
+    }
+
+}
