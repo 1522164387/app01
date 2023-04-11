64c550194ba9a07b61e3b12bcf344d2adf0f1bdc
1522164387 <1522164387@qq.com>
2023-04-11 22:48:08 +0800

    app14

--- cn.jnu.edu.x2020101335/app/src/main/java/com/example/cnjnuedux2020101335/game.java
+++ cn.jnu.edu.x2020101335/app/src/main/java/com/example/cnjnuedux2020101335/game.java
@@ -16,12 +16,20 @@ public class game {
             {
                 score += 10 + rolls[frameIndex + 2];
                 frameIndex += 2;
-            }else {
+            }else if(isStrike(frameIndex)){
+                score += 10 +rolls[frameIndex+1]+rolls[frameIndex + 2];
+                frameIndex += 1;
+            }
+            else {
                 score += rolls[frameIndex] + rolls[frameIndex + 1];
                 frameIndex += 2;
             }
         }
         return score;
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex]==10;
     }
 
     private boolean isSpare(int frameIndex) {
