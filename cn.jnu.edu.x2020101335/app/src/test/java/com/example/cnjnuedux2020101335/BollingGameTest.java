415b6297e90bb4bc14abf722bd02d662929d2614
1522164387 <1522164387@qq.com>
2023-04-11 22:48:50 +0800

    app15

--- cn.jnu.edu.x2020101335/app/src/test/java/com/example/cnjnuedux2020101335/BollingGameTest.java
+++ cn.jnu.edu.x2020101335/app/src/test/java/com/example/cnjnuedux2020101335/BollingGameTest.java
@@ -57,4 +57,9 @@ public class BollingGameTest {
         rollMany(16, 0);
         assertEquals(24, g.score());
     }
+    @Test
+    public void test5()  {
+        rollMany(12,10);
+        assertEquals(300, g.score());
+    }
 }
